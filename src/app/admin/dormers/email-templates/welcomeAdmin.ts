b062aa7735403ec7124940aae676d695659ccc4b
Pauline Dejos <22-1-00454@vsu.edu.ph>
2026-01-28 21:18:54 +0800

    Add user guide attachments to email messages; Apply semester billing period

--- src/app/admin/dormers/email-templates/welcomeAdmin.ts
+++ src/app/admin/dormers/email-templates/welcomeAdmin.ts
@@ -7,6 +7,7 @@ export const welcomeAdminTemplate = (
   <p>We're inviting you to be an admin of <a href="https://dorm-payment-system.vercel.app/">DormPay System</a>. You can now log in with the following credentials:</p>
   <p>Email: <strong>${email}</strong></p>
   <p>Password: <strong>${temporaryPassword}</strong></p>
+  <p>Please refer to the admin user guide attached below for detailed instructions on how to use the system.</p>
   <p style="margin-top: 25px;">Best regards,<br><strong>DormPay System</strong></p>
   <div style="border-top: 1px solid #eeeeee; margin-top: 30px; padding-top: 20px; color: #888888; text-align: center; font-size: 12px; line-height: 1.5;">
       <p style="margin: 0;">© ${new Date().getFullYear()} DormPay System. All rights reserved.</p>
--- src/app/admin/dormers/email-templates/welcomeUser.ts
+++ src/app/admin/dormers/email-templates/welcomeUser.ts
@@ -9,6 +9,7 @@ export const welcomeUserTemplate = (
   <p>Password: <strong>${temporaryPassword}</strong></p>
   <p style="margin-top: 25px;">Please <a href="https://dorm-payment-system.vercel.app/" style="color: #007bff; text-decoration: none;">log in</a> and change your password as soon as possible for security reasons.</p>
   <p>This is where you will receive your bills and payment confirmations!</p>
+  <p>Please refer to the user guide attached below for detailed instructions on how to use the system.</p>
   <p style="margin-top: 25px;">Best regards,<br><strong>VSU DormPay System</strong></p>
   <div style="border-top: 1px solid #eeeeee; margin-top: 30px; padding-top: 20px; color: #888888; text-align: center; font-size: 12px; line-height: 1.5;">
       <p style="margin: 0;">© ${new Date().getFullYear()} VSU DormPay System. All rights reserved.</p>
